3d8856e6381915965e4dfc2598cbee494a938a4b
mercede <mercedespontaut@gmail.com>
2025-04-14 11:41:47 -0300

    Algunos ej hechos

---
+++ Labo  1/prueba.py
@@ -0,0 +1,4 @@
+import numpy as np
+a =  np.array([[1,2,4],[1,2,3]])
+
+print(np.diag(np.ones(3),1) )
--- Labo1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# 0.25 es 2**-2
-# Es una cagada representar el 0.3. No se puede. 00111110100110011001100110011010
-
-
-# print(np.sqrt(2)**2-2 ) # Tiene el error de redondeo de la sqrt 
-
-print(1/(2*10**7))
